3e3eb63da18c15a46dad41a2f750fbed3d06d1f9
Prakarti <prabhakar29prakarti@gmail.com>
2025-03-05 23:02:33 +0530

    frontend added

---
+++ ems-frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import './App.css'
+import ListEmployeeComponent from './components/ListEmployeeComponent'
+import HeaderComponent from './components/HeaderComponent'
+import FooterComponent from './components/FooterComponent'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import EmployeeComponent from './components/EmployeeComponent'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <BrowserRouter>
+        <HeaderComponent />
+          <Routes>
+            {/* http://localhost:3000 */}
+            <Route path='/' element = {<ListEmployeeComponent />}></Route>
+            {/* http://localhost:3000/employees */}
+            <Route path='/employees' element = {<ListEmployeeComponent />}></Route>
+            {/* http://localhost:3000/add-employee */}
+            <Route path='/add-employee' element= {<EmployeeComponent />}></Route>
+            {/* http://localhost:3000/edit-employee/1 */}
+            <Route path='/edit-employee/:id' element= {<EmployeeComponent />}></Route>
+          </Routes>
+        <FooterComponent />
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
